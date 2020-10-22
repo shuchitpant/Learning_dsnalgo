f1b28459bf935b943643a14589b5645ec644857b
sbindal057 <shwetabindal2001@gmail.com>
2020-10-22 21:34:27 +0530

    Create arrange_negative_numbers.cpp

---
+++ Arrays/arrange_negative_numbers.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void arrange(int a[],int n)
+{
+  //by sorting the array in descending order all the negative elements will come to the left side of array.
+	sort(a,a+n);
+}
